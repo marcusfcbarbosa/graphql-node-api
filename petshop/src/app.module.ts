e81e75267fe4f05b484695f9c60a5b2e2fb5da59
Your Name <you@example.com>
2020-05-07 17:26:20 -0300

    Funcionando os 4 módulos

--- petshop/src/app.module.ts
+++ petshop/src/app.module.ts
@@ -5,11 +5,24 @@ import { AgendaModule } from './agenda/agenda.module';
 import { BackofficeModule } from './backoffice/backoffice.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StoreModule } from './store/store.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.CONNECTION_STRING),
-    AgendaModule, BackofficeModule
+    TypeOrmModule.forRoot(
+      {
+        type: 'mysql',
+        host: '127.0.0.1',
+        port: 3306,
+        username: 'root',
+        password: 'mfcb4625',
+        database: '7180',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true
+      }
+    ),
+    AgendaModule, BackofficeModule, StoreModule
   ],
   controllers: [AppController],
   providers: [AppService],
