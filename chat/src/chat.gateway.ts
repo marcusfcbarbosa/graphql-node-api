9711e7305c80873dad68708ce6d0e1bd154d3f72
Your Name <you@example.com>
2020-05-08 13:44:06 -0300

    Conexoes e desconexoes do websocket

---
+++ chat/src/chat.gateway.ts
@@ -0,0 +1,39 @@
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage } from '@nestjs/websockets'
+
+@WebSocketGateway(5000)//pode definir a porta que vai estar usando no seu servidor
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    //Definimos um servidor aqui dentro
+    @WebSocketServer()
+
+    handleConnection(client: any) {
+        console.log(client.id + ' conectando ..');
+
+        client.emit('connection', 'Conectado ao servidor..');
+
+        client.broadcast.emit('users', {
+            user: client.id, //Voce emite uma mensagem para um cliente especifico
+            action: 'connected'
+        });
+    }
+
+    handleDisconnect(client: any) {
+        console.log(client.id + ' desconectando ..');
+        client.broadcast.emit('users', {
+            user: client.id, //Voce emite uma mensagem para um cliente especifico
+            action: 'disconnected'
+        });
+    }
+
+
+    @SubscribeMessage('chat')
+    chat(client: any, data: any) {
+        //cliente emite para usuarios internos
+    }
+
+    @SubscribeMessage('users')
+    users(client: any, data: any) {
+        //usuarios internos para os clientes
+
+    }
+
+}
